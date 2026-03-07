9984ab879aa46470b67dba0947c0678c5da7f978
zamtrix-international <contact@zamtrix.in>
2026-03-07 22:04:52 +0530

    Update vendor.routes.js

--- Backend/src/routes/vendor.routes.js
+++ Backend/src/routes/vendor.routes.js
@@ -1,33 +1,43 @@
 /**
  * Vendor Routes
  */
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const vendorController = require('../controllers/vendor.controller');
-const auth = require('../middlewares/auth.middleware');
-const rbac = require('../middlewares/rbac.middleware');
-const upload = require('../middlewares/upload.middleware');
+
+const vendorController = require("../controllers/vendor.controller");
+const { auth } = require("../middlewares/auth.middleware");
+const { rbac } = require("../middlewares/rbac.middleware");
+const { upload } = require("../middlewares/upload.middleware");
 
 // Create vendor profile
-router.post('/', auth, rbac(['VENDOR']), vendorController.createVendorProfile);
+router.post("/", auth, rbac("VENDOR"), vendorController.createVendorProfile);
 
 // Get vendor profile
-router.get('/:vendorId', vendorController.getVendorProfile);
+router.get("/:vendorId", vendorController.getVendorProfile);
 
 // Update vendor profile
-router.put('/:vendorId', auth, rbac(['VENDOR']), vendorController.updateVendorProfile);
+router.put(
+  "/:vendorId",
+  auth,
+  rbac("VENDOR"),
+  vendorController.updateVendorProfile
+);
 
 // Upload KYC document
 router.post(
-  '/:vendorId/kyc',
+  "/:vendorId/kyc",
   auth,
-  rbac(['VENDOR']),
-  upload.single('document'),
+  rbac("VENDOR"),
+  upload.single("document"),
   vendorController.uploadKycDocument
 );
 
 // Add bank account
-router.post('/:vendorId/bank-account', auth, rbac(['VENDOR']), vendorController.addBankAccount);
+router.post(
+  "/:vendorId/bank-account",
+  auth,
+  rbac("VENDOR"),
+  vendorController.addBankAccount
+);
 
 module.exports = router;
