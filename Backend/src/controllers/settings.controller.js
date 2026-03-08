5c62f1c6f776422f990a060ee51f11fe0f179303
zamtrix-international <contact@zamtrix.in>
2026-03-08 18:24:15 +0530

    Add settings controller for payment and app settings

---
+++ Backend/src/controllers/settings.controller.js
@@ -0,0 +1,41 @@
+const asyncHandler = require("../utils/asyncHandler");
+const apiResponse = require("../utils/apiResponse");
+const settingsService = require("../services/settings.service");
+
+const getPaymentGatewayConfig = asyncHandler(async (req, res) => {
+  const configs = await settingsService.getPaymentGatewayConfig({
+    gateway: req.query.gateway,
+  });
+
+  return res.status(200).json(apiResponse(200, configs, "Payment gateway config retrieved"));
+});
+
+const updatePaymentGatewayConfig = asyncHandler(async (req, res) => {
+  const config = await settingsService.upsertPaymentGatewayConfig(
+    req.body,
+    req.user?.id
+  );
+
+  return res.status(200).json(apiResponse(200, config, "Payment gateway config updated"));
+});
+
+const getAppSettings = asyncHandler(async (req, res) => {
+  const settings = await settingsService.getAppSettings({
+    keyName: req.query.keyName,
+  });
+
+  return res.status(200).json(apiResponse(200, settings, "App settings retrieved"));
+});
+
+const updateAppSetting = asyncHandler(async (req, res) => {
+  const setting = await settingsService.upsertAppSetting(req.body, req.user?.id);
+
+  return res.status(200).json(apiResponse(200, setting, "App setting updated"));
+});
+
+module.exports = {
+  getPaymentGatewayConfig,
+  updatePaymentGatewayConfig,
+  getAppSettings,
+  updateAppSetting,
+};
