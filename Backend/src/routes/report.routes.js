2f60e0e9804ce5857972cca7ac705ea011117b43
zamtrix-international <contact@zamtrix.in>
2026-03-08 10:58:08 +0530

    Update report.routes.js

--- Backend/src/routes/report.routes.js
+++ Backend/src/routes/report.routes.js
@@ -2,22 +2,43 @@
  * Report Routes
  */
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const reportController = require('../controllers/report.controller');
-const auth = require('../middlewares/auth.middleware');
-const rbac = require('../middlewares/rbac.middleware');
+
+const reportController = require("../controllers/report.controller");
+const { auth } = require("../middlewares/auth.middleware");
+const { rbac } = require("../middlewares/rbac.middleware");
 
 // Generate booking report
-router.get('/booking', auth, rbac(['VENDOR']), reportController.generateBookingReport);
+router.get(
+  "/booking",
+  auth,
+  rbac("VENDOR"),
+  reportController.generateBookingReport
+);
 
 // Export report
-router.post('/export', auth, rbac(['VENDOR']), reportController.exportReport);
+router.post(
+  "/export",
+  auth,
+  rbac("VENDOR"),
+  reportController.exportReport
+);
 
 // Get export status
-router.get('/export/:exportId', auth, rbac(['VENDOR']), reportController.getExportStatus);
+router.get(
+  "/export/:exportId",
+  auth,
+  rbac("VENDOR"),
+  reportController.getExportStatus
+);
 
 // Get vendor reports
-router.get('/', auth, rbac(['VENDOR']), reportController.getVendorReports);
+router.get(
+  "/",
+  auth,
+  rbac("VENDOR"),
+  reportController.getVendorReports
+);
 
 module.exports = router;
