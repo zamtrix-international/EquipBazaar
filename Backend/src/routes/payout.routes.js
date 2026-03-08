fc7c1704dc7ce2344d7daa7d5d45ff2fda0b65a8
zamtrix-international <contact@zamtrix.in>
2026-03-08 10:56:36 +0530

    Update payout.routes.js

--- Backend/src/routes/payout.routes.js
+++ Backend/src/routes/payout.routes.js
@@ -2,22 +2,42 @@
  * Payout Routes
  */
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const payoutController = require('../controllers/payout.controller');
-const auth = require('../middlewares/auth.middleware');
-const rbac = require('../middlewares/rbac.middleware');
+
+const payoutController = require("../controllers/payout.controller");
+const { auth } = require("../middlewares/auth.middleware");
+const { rbac } = require("../middlewares/rbac.middleware");
 
 // Create withdrawal request
-router.post('/withdrawal-request', auth, rbac(['VENDOR']), payoutController.createWithdrawalRequest);
+router.post(
+  "/withdrawal-request",
+  auth,
+  rbac("VENDOR"),
+  payoutController.createWithdrawalRequest
+);
 
 // Get withdrawal request
-router.get('/withdrawal-request/:requestId', auth, payoutController.getWithdrawalRequest);
+router.get(
+  "/withdrawal-request/:requestId",
+  auth,
+  payoutController.getWithdrawalRequest
+);
 
 // Get vendor payouts
-router.get('/', auth, rbac(['VENDOR']), payoutController.getVendorPayouts);
+router.get(
+  "/",
+  auth,
+  rbac("VENDOR"),
+  payoutController.getVendorPayouts
+);
 
 // Process withdrawal (admin only)
-router.patch('/withdrawal-request/:requestId/process', auth, rbac(['ADMIN']), payoutController.processWithdrawal);
+router.patch(
+  "/withdrawal-request/:requestId/process",
+  auth,
+  rbac("ADMIN"),
+  payoutController.processWithdrawal
+);
 
 module.exports = router;
