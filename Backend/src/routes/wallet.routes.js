51b5eb68b5875cebd821c1316a0d4e88b3ac3fcd
zamtrix-international <contact@zamtrix.in>
2026-03-08 10:55:12 +0530

    Update wallet.routes.js

--- Backend/src/routes/wallet.routes.js
+++ Backend/src/routes/wallet.routes.js
@@ -2,19 +2,35 @@
  * Wallet Routes
  */
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const walletController = require('../controllers/wallet.controller');
-const auth = require('../middlewares/auth.middleware');
-const rbac = require('../middlewares/rbac.middleware');
+
+const walletController = require("../controllers/wallet.controller");
+const { auth } = require("../middlewares/auth.middleware");
+const { rbac } = require("../middlewares/rbac.middleware");
 
 // Get wallet balance
-router.get('/balance', auth, rbac(['VENDOR']), walletController.getWalletBalance);
+router.get(
+  "/balance",
+  auth,
+  rbac("VENDOR"),
+  walletController.getWalletBalance
+);
 
 // Get wallet ledger
-router.get('/ledger', auth, rbac(['VENDOR']), walletController.getWalletLedger);
+router.get(
+  "/ledger",
+  auth,
+  rbac("VENDOR"),
+  walletController.getWalletLedger
+);
 
 // Add funds to wallet
-router.post('/add-funds', auth, rbac(['VENDOR']), walletController.addWalletFunds);
+router.post(
+  "/add-funds",
+  auth,
+  rbac("VENDOR"),
+  walletController.addWalletFunds
+);
 
 module.exports = router;
